df515a7da3b95c0832c3f533cb05f7f15865b72e
AhmedFathy114 <af3611038@gmail.com>
2026-04-26 20:19:04 +0300

    complete Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "./components/footer/footer";
+import { Main } from "./components/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
